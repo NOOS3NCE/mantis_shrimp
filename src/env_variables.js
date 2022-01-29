018520af869d40a8f8a4fa03561434347fde59a4
NOOS3NCE <83935244+NOOS3NCE@users.noreply.github.com>
2022-01-28 23:32:48 -0600

    fixed scroll bars and margin on page container

--- src/env_variables.js
+++ src/env_variables.js
@@ -2,4 +2,4 @@ require('dotenv').config()
 console.log(process.env.NODE_ENV)
 
 const env = process.env.NODE_ENV
-export const base_url = 'https://wildorchid.one/'
+export const base_url = env === 'development' ? 'http://localhost:3001/' : 'https://wildorchid.one/'
