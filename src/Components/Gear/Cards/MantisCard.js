3252e6f2f8220eb33f45411b61d115015279b984
NOOS3NCE <83935244+NOOS3NCE@users.noreply.github.com>
2022-01-28 20:34:20 -0600

    made entire list item link in MantisCard.js

--- src/Components/Gear/Cards/MantisCard.js
+++ src/Components/Gear/Cards/MantisCard.js
@@ -6,52 +6,48 @@ const MantisCard = (props) => {
 
     return (
         <>
-            <div
-                className={'main-list zoom p-0 px-2 my-1 d-flex align-items-center justify-content-between'}>
-
-                <div className={'col-12 d-flex flex-row align-items-center justify-content-start'}>
-
-                    <div className={'col-2'} style={{minWidth: '55px'}}>
-                        <div
-                            className={'bg-pureWhite rounded img-fluid d-flex align-items-center justify-content-center'}
-                            style={{height: '55px', width: '55px'}}>
-                            <Link className={'text-decoration-none'} to={`/gear/pack/${id}`}>
+            <Link className={'text-decoration-none'} to={`/gear/pack/${id}`}>
+                <div
+                    className={'main-list zoom p-0 px-2 my-1 d-flex align-items-center justify-content-between'}>
+                    <div className={'col-12 d-flex flex-row align-items-center justify-content-start'}>
+                        <div className={'col-2'} style={{minWidth: '55px'}}>
+                            <div
+                                className={'bg-pureWhite rounded img-fluid d-flex align-items-center justify-content-center'}
+                                style={{height: '55px', width: '55px'}}>
                                 <img
                                     src={data?.hasOwnProperty('kit_status') ? data.kit_img : data.lens_img}
                                     alt={"SVG of backpack"} style={{height: '45px'}}/>
-                            </Link>
+                            </div>
+                        </div>
+                        <div className={'col-10 d-flex align-items-center justify-content-start'}>
+                            {data?.lens_display && <div className={'col m-0 p-0'}>
+                                <p>{data?.lens_display.toUpperCase()}</p>
+                            </div>}
+                            {data?.kit_display && <div className={'col m-0 p-0'}>
+                                <p>{data?.kit_display.toUpperCase()}</p>
+                            </div>}
+                            {data?.lens_brand && <div className={'col m-0 p-0'}>
+                                <p>{data?.lens_brand}</p>
+                            </div>}
+                            {data?.lens_model && <div className={'col m-0 p-0'}>
+                                <p>{data?.lens_model}mm</p>
+                            </div>}
+                            {data?.city_code && <div className={'col m-0 p-0'}>
+                                <p>{data?.city_code}</p>
+                            </div>}
+                            {data?.kit_type && <div className={'col m-0 p-0'}>
+                                <p>{data?.kit_type}</p>
+                            </div>}
+                            {data?.kit_status && <div className={'col m-0 p-0'}>
+                                <p>{data?.kit_status}</p>
+                            </div>}
+                            {data?.hasOwnProperty('user_firstname') &&
+                            <div className={'col m-0 p-0'}><p>{data?.user_firstname}</p>
+                            </div>}
                         </div>
                     </div>
-                    <div className={'col-10 d-flex align-items-center justify-content-start'}>
-                        {data?.lens_display && <div className={'col m-0 p-0'}>
-                            <p>{data?.lens_display.toUpperCase()}</p>
-                        </div>}
-                        {data?.kit_display && <div className={'col m-0 p-0'}>
-                            <p>{data?.kit_display.toUpperCase()}</p>
-                        </div>}
-                        {data?.lens_brand && <div className={'col m-0 p-0'}>
-                            <p>{data?.lens_brand}</p>
-                        </div>}
-                        {data?.lens_model && <div className={'col m-0 p-0'}>
-                            <p>{data?.lens_model}mm</p>
-                        </div>}
-                        {data?.city_code && <div className={'col m-0 p-0'}>
-                            <p>{data?.city_code}</p>
-                        </div>}
-                        {data?.kit_type && <div className={'col m-0 p-0'}>
-                            <p>{data?.kit_type}</p>
-                        </div>}
-                        {data?.kit_status && <div className={'col m-0 p-0'}>
-                            <p>{data?.kit_status}</p>
-                        </div>}
-                        {data?.hasOwnProperty('user_firstname') &&
-                        <div className={'col m-0 p-0'}><p>{data?.user_firstname}</p>
-                        </div>}
-                    </div>
-
                 </div>
-
-            </div>
+            </Link>
         </>
     )
 }
